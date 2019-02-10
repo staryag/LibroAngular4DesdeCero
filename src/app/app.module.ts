c3b1c5953b19c9fe0359feeb82d802017457f96a
staryag <staryag@gmail.com>
2019-02-10 20:12:29 +0100

    Próxima lección: 4.Data Binding

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FechaactualComponent } from './fechaactual/fechaactual.component';
+import { CopyrightComponent } from './copyright/copyright.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FechaactualComponent,
+    CopyrightComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
